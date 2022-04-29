1188eef7b453053d5d934a8010af839d1c792a53
Kevin Beam <14251936+kbeamnsidc@users.noreply.github.com>
2022-04-29 17:25:20 -0600

    Update nvim config

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -8,23 +8,23 @@ end
 
 local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
 vim.api.nvim_create_autocmd(
-  'BufWritePost', { 
-    command = 'source <afile> | PackerSync', 
-    group = packer_group, 
-    pattern = 'init.lua' 
-  })
+  'BufWritePost', {
+  command = 'source <afile> | PackerSync',
+  group = packer_group,
+  pattern = 'init.lua'
+})
 
 --
 -- Install packages
 --
 require('packer').startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
-  
-  use 'joshdick/onedark.vim'
+
   use 'christoomey/vim-tmux-navigator'
-  use { 
-    'nvim-telescope/telescope.nvim', 
-    requires = { 'nvim-lua/plenary.nvim' } 
+  use 'glepnir/dashboard-nvim'
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { 'nvim-lua/plenary.nvim' }
   }
   use {
     "folke/which-key.nvim",
@@ -33,45 +33,53 @@ require('packer').startup(function(use)
       }
     end
   }
+  use 'folke/tokyonight.nvim'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use 'hashivim/vim-terraform'
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/nvim-lsp-installer'
 end)
 
 --
 -- Basic options
 --
-vim.opt.backup = false                          -- creates a backup file
-vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
-vim.opt.cmdheight = 2                           -- more space in the neovim command line for displaying messages
+vim.opt.backup = false -- creates a backup file
+vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
+vim.opt.cmdheight = 2 -- more space in the neovim command line for displaying messages
 vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
-vim.opt.conceallevel = 0                        -- so that `` is visible in markdown files
-vim.opt.cursorline = false                       -- highlight the current line
-vim.opt.expandtab = true                        -- convert tabs to spaces
-vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
-vim.opt.guifont = "monospace:h17"               -- the font used in graphical neovim applications
-vim.opt.hlsearch = true                         -- highlight all matches on previous search pattern
-vim.opt.ignorecase = true                       -- ignore case in search patterns
-vim.opt.mouse = "a"                             -- allow the mouse to be used in neovim
-vim.opt.number = true                           -- set numbered lines
-vim.opt.numberwidth = 4                         -- set number column width to 2 {default 4}
-vim.opt.pumheight = 10                          -- pop up menu height
-vim.opt.relativenumber = false                  -- set relative numbered lines
-vim.opt.scrolloff = 8                           -- is one of my fav
-vim.opt.shiftwidth = 2                          -- the number of spaces inserted for each indentation
-vim.opt.showmode = false                        -- we don't need to see things like -- INSERT -- anymore
-vim.opt.showtabline = 2                         -- always show tabs
+vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
+vim.opt.cursorline = false -- highlight the current line
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.fileencoding = "utf-8" -- the encoding written to a file
+vim.opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
+vim.opt.hlsearch = true -- highlight all matches on previous search pattern
+vim.opt.ignorecase = true -- ignore case in search patterns
+vim.opt.mouse = "a" -- allow the mouse to be used in neovim
+vim.opt.number = true -- set numbered lines
+vim.opt.numberwidth = 4 -- set number column width to 2 {default 4}
+vim.opt.pumheight = 10 -- pop up menu height
+vim.opt.relativenumber = false -- set relative numbered lines
+vim.opt.scrolloff = 8 -- is one of my fav
+vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
+vim.opt.showmode = false -- we don't need to see things like -- INSERT -- anymore
+vim.opt.showtabline = 2 -- always show tabs
 vim.opt.sidescrolloff = 8
-vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
-vim.opt.smartcase = true                        -- smart case
-vim.opt.smartindent = true                      -- make indenting smarter again
-vim.opt.splitbelow = true                       -- force all horizontal splits to go below current window
-vim.opt.splitright = true                       -- force all vertical splits to go to the right of current window
-vim.opt.swapfile = false                        -- creates a swapfile
-vim.opt.tabstop = 2                             -- insert 2 spaces for a tab
-vim.opt.termguicolors = true                    -- set term gui colors (most terminals support this)
-vim.opt.timeoutlen = 100                        -- time to wait for a mapped sequence to complete (in milliseconds)
-vim.opt.undofile = true                         -- enable persistent undo
-vim.opt.updatetime = 250                        -- faster completion (4000ms default)
-vim.opt.wrap = false                            -- display lines as one long line
-vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
+vim.opt.smartcase = true -- smart case
+vim.opt.smartindent = true -- make indenting smarter again
+vim.opt.splitbelow = true -- force all horizontal splits to go below current window
+vim.opt.splitright = true -- force all vertical splits to go to the right of current window
+vim.opt.swapfile = false -- creates a swapfile
+vim.opt.tabstop = 2 -- insert 2 spaces for a tab
+vim.opt.termguicolors = true -- set term gui colors (most terminals support this)
+vim.opt.timeoutlen = 100 -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.opt.undofile = true -- enable persistent undo
+vim.opt.updatetime = 250 -- faster completion (4000ms default)
+vim.opt.wrap = false -- display lines as one long line
+vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 vim.opt.shortmess:append "c"
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
@@ -79,22 +87,68 @@ vim.g.netrw_liststyle = 3
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 
-vim.cmd "colorscheme onedark"
-
-local opts = { noremap = true, silent = true }
-local term_opts = { silent = true }
-local keymap = vim.api.nvim_set_keymap
-
 vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-require('telescope').setup { }
+vim.g.dashboard_default_executive = 'telescope'
 
+require('telescope').setup {}
 vim.cmd "nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>"
 vim.cmd "nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>"
 vim.cmd "nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>"
 vim.cmd "nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>"
+
+vim.g.tokyonight_style = "night"
+vim.cmd "colorscheme tokyonight"
+
+require('lualine').setup()
+
+require("nvim-lsp-installer").setup {}
+
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>p', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { 'asm_lsp', 'bashls', 'dockerls', 'fortls', 'jsonls', 'remark_ls', 'pylsp', 'sumneko_lua', 'terraformls', 'tsserver', 'yamlls' }
+for _, lsp in pairs(servers) do
+  require('lspconfig')[lsp].setup {
+    on_attach = on_attach,
+    flags = {
+      -- This will be the default in neovim 0.7+
+      debounce_text_changes = 150,
+    }
+  }
+end
 
 -- Modes
 --   normal_mode = "n",
@@ -103,6 +157,10 @@ vim.cmd "nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<c
 --   visual_block_mode = "x",
 --   term_mode = "t",
 --   command_mode = "c",
+--
+opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
+local keymap = vim.api.nvim_set_keymap
 
 -- Normal --
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
